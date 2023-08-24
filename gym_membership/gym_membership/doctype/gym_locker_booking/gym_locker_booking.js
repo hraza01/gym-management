1ddb0551254cb27eb0b10dff8d2da97678a3ad5c
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2023-08-24 18:45:52 -0400

    feat: locker restriction

--- gym_membership/gym_membership/doctype/gym_locker_booking/gym_locker_booking.js
+++ gym_membership/gym_membership/doctype/gym_locker_booking/gym_locker_booking.js
@@ -1,8 +1,36 @@
 // Copyright (c) 2023, Hasan Raza and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Gym Locker Booking", {
-// 	refresh(frm) {
+frappe.ui.form.on("Gym Locker Booking", {
+	refresh(frm) {
+    frm.add_custom_button("Find Available Lockers", () => {
+      frappe.call({
+        method: 'gym_membership.gym_membership.doctype.gym_locker_booking.gym_locker_booking.get_available_lockers',
+        args: {
+          start_time: frm.doc.start_time,
+          duration: frm.doc.duration
+        }
+      }).then(res => {
+        let dialog = new frappe.ui.Dialog({
+          title: "Find a Locker",
+          fields: [
+            {
+              fieldtype: "Select",
+              fieldname: "locker",
+              label: "Locker",
+              options: res.message.map(r => r.name)
+            },
+          ],
+          primary_action_label: "Select",
+          primary_action: (data) => {
+            let { locker } = data
+            frm.set_value('locker', locker)
+            dialog.hide()
+          }
+        })
 
-// 	},
-// });
+        dialog.show();
+      })
+    })
+	},
+});
--- gym_membership/gym_membership/doctype/gym_locker_booking/gym_locker_booking.py
+++ gym_membership/gym_membership/doctype/gym_locker_booking/gym_locker_booking.py
@@ -1,9 +1,54 @@
 # Copyright (c) 2023, Hasan Raza and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.query_builder import Query, AliasedQuery
+
+import datetime as dt
 
 
 class GymLockerBooking(Document):
-	pass
+    def before_save(self):
+        end_time = get_end_time(self.start_time, self.duration)
+        self.end_time = str(end_time)
+
+
+def get_end_time(start_time, duration):
+    return dt.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S") + dt.timedelta(
+        minutes=int(duration)
+    )
+
+
+@frappe.whitelist()
+def get_available_lockers(start_time, duration):
+    gym_locker_bookings = frappe.qb.DocType("Gym Locker Booking")
+    gym_lockers = frappe.qb.DocType("Gym Locker")
+
+    start = dt.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+    end = get_end_time(start_time, duration)
+
+    # ALL BOOKED LOCKERS FOR THE SPECIFIED TIMEFRAME
+    sub_query = (
+        frappe.qb.from_(gym_locker_bookings)
+        .select("locker")
+        .distinct()
+        .where(gym_locker_bookings.start_time >= start)
+        .where(gym_locker_bookings.end_time <= end)
+    ).run(as_dict=True)
+
+    booked_lockers = [item.locker for item in sub_query]
+
+    if not booked_lockers:
+        return (frappe.qb.from_(gym_lockers).select("name").distinct()).run(
+            as_dict=True
+        )
+
+    query = (
+        frappe.qb.from_(gym_lockers)
+        .select("name")
+        .distinct()
+        .where(gym_lockers.name.notin(booked_lockers))
+    )
+
+    return query.run(as_dict=True)
