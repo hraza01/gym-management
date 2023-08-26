bcf8d05e81d981053c31b6b578c15c58ef1ebb63
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2023-08-26 19:52:23 -0400

    feat: gym class registration

--- gym_membership/gym_membership/doctype/gym_class/gym_class.py
+++ gym_membership/gym_membership/doctype/gym_class/gym_class.py
@@ -2,11 +2,15 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.model.document import Document
+from frappe.website.website_generator import WebsiteGenerator
 from gym_membership.utils.helpers import get_end_time
+import re
 
 
-class GymClass(Document):
+class GymClass(WebsiteGenerator):
     def before_save(self):
         end_time = get_end_time(self.start_time, self.duration)
+        route = re.sub(r'[^A-Za-z0-9-]', '-', self.name)
+
         self.end_time = str(end_time)
+        self.route = f"class/{route.lower()}"
---
+++ gym_membership/gym_membership/web_form/new_class_registration/new_class_registration.js
@@ -0,0 +1,21 @@
+frappe.ready(function () {
+  frappe.web_form.on('member_id', (field, value) => {
+    frappe.call({
+      method: 'gym_membership.gym_membership.doctype.gym_membership.gym_membership.get_membership_detail',
+      args: {
+        member_id: value,
+      }
+    }).then(res => {
+      try {
+        const [{name: membership_no}] = res.message
+        frappe.web_form.set_value('membership_no', membership_no)
+
+      } catch (e) {
+        frappe.web_form.set_value('membership_no', '')
+        frappe.throw(`Unable to find an active membership for member ${value}`, "Membership Not Found")
+      }
+    })
+  })
+})
+
+
