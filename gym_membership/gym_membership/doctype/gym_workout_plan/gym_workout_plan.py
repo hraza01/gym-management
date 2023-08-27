0f0fc84bbcafb7ad5b1aca3729cdffb834cdca0e
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2023-08-26 21:02:12 -0400

    feat: total_burn & total_duration

--- gym_membership/gym_membership/doctype/gym_workout_plan/gym_workout_plan.py
+++ gym_membership/gym_membership/doctype/gym_workout_plan/gym_workout_plan.py
@@ -6,4 +6,17 @@ from frappe.model.document import Document
 
 
 class GymWorkoutPlan(Document):
-	pass
+
+    def before_save(self):
+        self.total_duration, self.total_burn = get_totals(self.workouts)
+
+
+def get_totals(workouts):
+    total_duration = 0
+    total_burn = 0
+
+    for workout in workouts:
+        total_duration += workout.duration_minutes
+        total_burn += (workout.calorie_burn_rate / 60) * workout.duration_minutes
+
+    return total_duration, total_burn
