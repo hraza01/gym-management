a0b1dbd0ac9ba70213aa565247d4d9ffc7bbef6a
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2023-08-29 16:57:49 -0400

    added unit tests

--- gym_membership/gym_membership/doctype/gym_member/gym_member.py
+++ gym_membership/gym_membership/doctype/gym_member/gym_member.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023, Hasan Raza and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 import datetime as dt
 
@@ -9,20 +9,33 @@ import datetime as dt
 class GymMember(Document):
     def get_full_name(self):
         """Returns the passenger's full name"""
+        if not self.last_name:
+            return self.first_name
+
         return f"{self.first_name} {self.last_name}"
 
     def get_age(self):
         """Returns the age given the date of birth"""
         today = dt.date.today()
-        parsed_dob = dt.datetime.strptime(self.date_of_birth, "%Y-%m-%d")
+        dob = get_parsed_date(self.date_of_birth)
         age = (
             today.year
-            - parsed_dob.year
-            - ((today.month, today.day) < (parsed_dob.month, parsed_dob.day))
+            - dob.year
+            - ((today.month, today.day) < (dob.month, dob.day))
         )
 
         return age
 
     def before_save(self):
+
+        dob = get_parsed_date(self.date_of_birth)
+
+        if dob.date() > dt.date.today():
+            frappe.throw("Date of Birth cannot be greater than today.")
+
         self.full_name = self.get_full_name()
         self.age = self.get_age()
+
+
+def get_parsed_date(date):
+    return dt.datetime.strptime(date, "%Y-%m-%d")
--- gym_membership/gym_membership/doctype/gym_member/test_gym_member.py
+++ gym_membership/gym_membership/doctype/gym_member/test_gym_member.py
@@ -1,9 +1,61 @@
 # Copyright (c) 2023, Hasan Raza and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
+import datetime as dt
 
 
 class TestGymMember(FrappeTestCase):
-	pass
+    def test_fullname_is_set_automatically(self):
+        test_member = frappe.get_doc({
+            "doctype": "Gym Member",
+            "first_name": "Tony",
+            "last_name": "Smith",
+            "date_of_birth": "1995-06-26",
+            "email": "tony.smith@example.com",
+            "phone": "9901234123"
+        }).insert()
+
+        self.assertEqual(test_member.full_name, "Tony Smith")
+
+    def test_fullname_is_set_correctly_if_no_last_name(self):
+        test_member = frappe.get_doc({
+            "doctype": "Gym Member",
+            "first_name": "Tony",
+            "date_of_birth": "1995-06-26",
+            "email": "tony.smith@example.com",
+            "phone": "9901234123"
+        }).insert()
+
+        self.assertEqual(test_member.full_name, "Tony")
+
+    def test_age_is_set_automatically(self):
+        test_member = frappe.get_doc({
+            "doctype": "Gym Member",
+            "first_name": "Tony",
+            "last_name": "Smith",
+            "date_of_birth": "1995-06-26",
+            "email": "tony.smith@example.com",
+            "phone": "9901234123"
+        }).insert()
+
+        self.assertEqual(test_member.age, 28)
+
+    def test_dob_not_greater_than_today(self):
+        test_member = frappe.get_doc({
+            "doctype": "Gym Member",
+            "first_name": "Tony",
+            "last_name": "Smith",
+            "date_of_birth": str(dt.date.today() + dt.timedelta(days=1)),
+            "email": "tony.smith@example.com",
+            "phone": "9901234123"
+        })
+
+        with self.assertRaises(frappe.exceptions.ValidationError) as context:
+            test_member.insert()
+
+        self.assertTrue('Date of Birth cannot be greater than today' in str(context.exception))
+
+
+
