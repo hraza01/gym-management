aabedf3ebf5384e93145f1fe106b36c7444afdb0
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2023-09-19 21:44:43 -0400

    app reclass

--- gym_management/gym_management/doctype/gym_class/gym_class.py
+++ gym_management/gym_management/doctype/gym_class/gym_class.py
@@ -3,14 +3,15 @@
 
 # import frappe
 from frappe.website.website_generator import WebsiteGenerator
-from gym_management.utils.helpers import get_end_time
+# from gym_management.utils.helpers import get_end_time
 import re
 
 
 class GymClass(WebsiteGenerator):
-    def before_save(self):
-        end_time = get_end_time(self.start_time, self.duration)
-        route = re.sub(r'[^A-Za-z0-9-]', '-', self.name)
-
-        self.end_time = str(end_time)
-        self.route = f"class/{route.lower()}"
+    pass
+    # def before_save(self):
+    #     end_time = get_end_time(self.start_time, self.duration)
+    #     route = re.sub(r'[^A-Za-z0-9-]', '-', self.name)
+    #
+    #     self.end_time = str(end_time)
+    #     self.route = f"class/{route.lower()}"
--- gym_management/gym_management/doctype/gym_locker_booking/gym_locker_booking.py
+++ gym_management/gym_management/doctype/gym_locker_booking/gym_locker_booking.py
@@ -3,45 +3,46 @@
 
 import frappe
 from frappe.model.document import Document
-from gym_membership.utils.helpers import get_end_time
+# from gym_management.utils.helpers import get_end_time
 import datetime as dt
 
 
 class GymLockerBooking(Document):
-    def before_save(self):
-        end_time = get_end_time(self.start_time, self.duration)
-        self.end_time = str(end_time)
+    pass
+    # def before_save(self):
+    #     end_time = get_end_time(self.start_time, self.duration)
+    #     self.end_time = str(end_time)
 
 
-@frappe.whitelist()
-def get_available_lockers(start_time, duration):
-    gym_locker_bookings = frappe.qb.DocType("Gym Locker Booking")
-    gym_lockers = frappe.qb.DocType("Gym Locker")
-
-    start = dt.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
-    end = get_end_time(start_time, duration)
-
-    # ALL BOOKED LOCKERS FOR THE SPECIFIED TIMEFRAME
-    sub_query = (
-        frappe.qb.from_(gym_locker_bookings)
-        .select("locker")
-        .distinct()
-        .where(gym_locker_bookings.start_time >= start)
-        .where(gym_locker_bookings.end_time <= end)
-    ).run(as_dict=True)
-
-    booked_lockers = [item.locker for item in sub_query]
-
-    if not booked_lockers:
-        return (frappe.qb.from_(gym_lockers).select("name").distinct()).run(
-            as_dict=True
-        )
-
-    query = (
-        frappe.qb.from_(gym_lockers)
-        .select("name")
-        .distinct()
-        .where(gym_lockers.name.notin(booked_lockers))
-    )
-
-    return query.run(as_dict=True)
+# @frappe.whitelist()
+# def get_available_lockers(start_time, duration):
+#     gym_locker_bookings = frappe.qb.DocType("Gym Locker Booking")
+#     gym_lockers = frappe.qb.DocType("Gym Locker")
+#
+#     start = dt.datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S")
+#     end = get_end_time(start_time, duration)
+#
+#     # ALL BOOKED LOCKERS FOR THE SPECIFIED TIMEFRAME
+#     sub_query = (
+#         frappe.qb.from_(gym_locker_bookings)
+#         .select("locker")
+#         .distinct()
+#         .where(gym_locker_bookings.start_time >= start)
+#         .where(gym_locker_bookings.end_time <= end)
+#     ).run(as_dict=True)
+#
+#     booked_lockers = [item.locker for item in sub_query]
+#
+#     if not booked_lockers:
+#         return (frappe.qb.from_(gym_lockers).select("name").distinct()).run(
+#             as_dict=True
+#         )
+#
+#     query = (
+#         frappe.qb.from_(gym_lockers)
+#         .select("name")
+#         .distinct()
+#         .where(gym_lockers.name.notin(booked_lockers))
+#     )
+#
+#     return query.run(as_dict=True)
