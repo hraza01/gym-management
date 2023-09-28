87e0d5bb51c517cfed16b061f892846b028311a5
Hasan Raza <hasan.raza@blackfrosttechnologies.com>
2023-09-28 13:54:51 -0400

    feat: fixtures exported for gym roles

--- gym_management/hooks.py
+++ gym_management/hooks.py
@@ -41,8 +41,15 @@ app_license = "mit"
 
 # website user home page (by Role)
 # role_home_page = {
-#	"Role": "home_page"
+# 	"Role": "home_page"
 # }
+
+fixtures = [
+    {
+        "doctype": "Role",
+        "filters": [["name", "in", ["Gym Admin", "Gym Member", "Gym Trainer"]]],
+    }
+]
 
 # Generators
 # ----------
@@ -55,8 +62,8 @@ app_license = "mit"
 
 # add methods and filters to jinja environment
 # jinja = {
-#	"methods": "gym_management.utils.jinja_methods",
-#	"filters": "gym_management.utils.jinja_filters"
+# 	"methods": "gym_management.utils.jinja_methods",
+# 	"filters": "gym_management.utils.jinja_filters"
 # }
 
 # Installation
@@ -98,11 +105,11 @@ app_license = "mit"
 # Permissions evaluated in scripted ways
 
 # permission_query_conditions = {
-#	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
 # }
 #
 # has_permission = {
-#	"Event": "frappe.desk.doctype.event.event.has_permission",
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
 # }
 
 # DocType Class
@@ -110,7 +117,7 @@ app_license = "mit"
 # Override standard doctype classes
 
 # override_doctype_class = {
-#	"ToDo": "custom_app.overrides.CustomToDo"
+# 	"ToDo": "custom_app.overrides.CustomToDo"
 # }
 
 # Document Events
@@ -118,32 +125,32 @@ app_license = "mit"
 # Hook on document methods and events
 
 # doc_events = {
-#	"*": {
-#		"on_update": "method",
-#		"on_cancel": "method",
-#		"on_trash": "method"
-#	}
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+# 	}
 # }
 
 # Scheduled Tasks
 # ---------------
 
 # scheduler_events = {
-#	"all": [
-#		"gym_management.tasks.all"
-#	],
-#	"daily": [
-#		"gym_management.tasks.daily"
-#	],
-#	"hourly": [
-#		"gym_management.tasks.hourly"
-#	],
-#	"weekly": [
-#		"gym_management.tasks.weekly"
-#	],
-#	"monthly": [
-#		"gym_management.tasks.monthly"
-#	],
+# 	"all": [
+# 		"gym_management.tasks.all"
+# 	],
+# 	"daily": [
+# 		"gym_management.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"gym_management.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"gym_management.tasks.weekly"
+# 	],
+# 	"monthly": [
+# 		"gym_management.tasks.monthly"
+# 	],
 # }
 
 # Testing
@@ -155,14 +162,14 @@ app_license = "mit"
 # ------------------------------
 #
 # override_whitelisted_methods = {
-#	"frappe.desk.doctype.event.event.get_events": "gym_management.event.get_events"
+# 	"frappe.desk.doctype.event.event.get_events": "gym_management.event.get_events"
 # }
 #
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
 # along with any modifications made in other Frappe apps
 # override_doctype_dashboards = {
-#	"Task": "gym_management.task.get_dashboard_data"
+# 	"Task": "gym_management.task.get_dashboard_data"
 # }
 
 # exempt linked doctypes from being automatically cancelled
@@ -188,29 +195,29 @@ app_license = "mit"
 # --------------------
 
 # user_data_fields = [
-#	{
-#		"doctype": "{doctype_1}",
-#		"filter_by": "{filter_by}",
-#		"redact_fields": ["{field_1}", "{field_2}"],
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_2}",
-#		"filter_by": "{filter_by}",
-#		"partial": 1,
-#	},
-#	{
-#		"doctype": "{doctype_3}",
-#		"strict": False,
-#	},
-#	{
-#		"doctype": "{doctype_4}"
-#	}
+# 	{
+# 		"doctype": "{doctype_1}",
+# 		"filter_by": "{filter_by}",
+# 		"redact_fields": ["{field_1}", "{field_2}"],
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_2}",
+# 		"filter_by": "{filter_by}",
+# 		"partial": 1,
+# 	},
+# 	{
+# 		"doctype": "{doctype_3}",
+# 		"strict": False,
+# 	},
+# 	{
+# 		"doctype": "{doctype_4}"
+# 	}
 # ]
 
 # Authentication and authorization
 # --------------------------------
 
 # auth_hooks = [
-#	"gym_management.auth.validate"
+# 	"gym_management.auth.validate"
 # ]
